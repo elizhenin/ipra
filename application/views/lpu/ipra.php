4bbfc43fbb2b0404b38df938e550c3d0014ecc9b
elizhenin <elizhenin@lsbr.ru>
2017-05-15 19:16:10 +0300

    lpu/ipra - add medorg_sender to csv

--- application/views/lpu/ipra.php
+++ application/views/lpu/ipra.php
@@ -72,7 +72,7 @@ foreach($med_org as $key=>$value){
                         });
                     }
                     if (event.target == 'csv') {
-                        var csv = 'Дата выдачи;Дата окончания;Фамилия;Иия;Отчество;Дата рождения;№ ИПРА;СНИЛС;Мед.организация;' + "\n";
+                        var csv = 'Дата выдачи;Дата окончания;Фамилия;Иия;Отчество;Дата рождения;№ ИПРА;СНИЛС;Мед.организация;Направившая МО;' + "\n";
                         for (i = 1; i <= w2ui.person_list.records.length; i++) {
                             csv = csv + w2ui.person_list.records[i - 1].prgdt + ';';
                             csv = csv + w2ui.person_list.records[i - 1].prgenddt + ';';
@@ -83,6 +83,7 @@ foreach($med_org as $key=>$value){
                             csv = csv + w2ui.person_list.records[i - 1].prgnum + ';';
                             csv = csv + w2ui.person_list.records[i - 1].snils.trim() + ';';
                             csv = csv + w2ui.person_list.records[i - 1].medorg.trim() + ';';
+                            csv = csv + w2ui.person_list.records[i - 1].medorg_sender.trim() + ';';
                             csv = csv + "\n";
                         }
                         csv = $('<div/>').html(csv).text();
